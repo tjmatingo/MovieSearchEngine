b29e4d828cd9cdefc32eb460ff0c5dcb9e4cfb22
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-08 21:44:56 +0200

    react extras deleted, movie card components created in home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import MovieCard from "../components/MovieCard";
+
+function Home() {
+    const movies = [
+        {id: 1, title: "How to Train a Dragon", release_date: "2025"},
+        {id: 2, title: "Avatar: The Last Air bender", release_date: "2012"},
+        {id: 3, title: "Shan-Chi", release_date: "2023"},
+        {id: 4, title: "Spiderman: Beyond the Spider Verse", release_date: "2028"},
+
+    ]
+
+    const handleSearch = () => {
+
+    }
+    
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" placeholder="Which Movie is it?" className="search-input" />
+            </form>
+            
+            <div className="movies-grid">
+                {movies.map(movie => (
+                    <MovieCard movie={movie} key={movie.id}></MovieCard>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Home;
