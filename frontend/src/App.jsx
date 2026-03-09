d28ef29ba78b3f7a9e86bdf43dcefc41479c155d
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-09 22:49:11 +0200

    navbar integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,21 @@ import './App.css'
 import Favourite from './pages/Favourites'
 import Home from './pages/Home'
 import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <main className='main-content'>
-      <Routes>
-        <Route path='/' element={<Home /> }/>
-        <Route path='/favourites' element={<Favourite /> }/>
-      </Routes>
-    </main>
+    <div>
+      <Navbar />
+
+      <main className='main-content'>  
+        <Routes>
+          <Route path='/' element={<Home /> }/>
+          <Route path='/favourites' element={<Favourite /> }/>
+        </Routes>
+      </main>
+    </div>
   )
 }
 
