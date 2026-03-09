c99dca1595843daabab7ea00524da2023ed35b00
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-09 22:32:17 +0200

    favourites page

---
+++ frontend/src/pages/Favourites.jsx
@@ -0,0 +1,8 @@
+function Favourite() {
+    return <div className="favourites-empty">
+        <h2>No Favourite movies yet😏</h2>
+        <p>Start adding some films to your Favourites List</p>
+    </div>
+};
+
+export default Favourite;
