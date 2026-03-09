afda4e0960c93076b6147714b95846805db1df06
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-09 22:46:36 +0200

    navbar component creation

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+
+function Navbar() {
+    return <nav className="navbar">
+        <div className="navbar-brand">
+            <Link to="/">Movie App</Link>
+        </div>
+        <div className="navbar-links">
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/favourites" className="nav-link">Favourites</Link>
+
+        </div>
+
+    </nav>
+};
+
+
+export default Navbar;
