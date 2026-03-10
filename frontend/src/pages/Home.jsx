05e5ae4c61388df71ac565e562fb40b7bec9e6bc
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-10 23:12:20 +0200

    state configuration for rerending and loading the movies from api to frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,37 @@
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../css/Home.css"
-import { useEffect } from "react";
+
 import { searchMovies, getPopularMovies } from "../services/api";
 
 function Home() {
     // states for the search bar and filtering fucntionality 
     const [searchQuery, setSearchQuery] = useState("");
-    
-    // dummy data for testing filter function
-    const movies = getPopularMovies()
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies();
+                setMovies(popularMovies);
+            } catch (err) {
+                console.log(err);
+                setError("failed to load....");
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        loadPopularMovies();
+    }, []);
+    
     // function for dealing with the form submission
     const handleSearch = (e) => {
         e.preventDefault();
         alert(searchQuery);
-    }
+    };
     
     return (
         <div className="home">
