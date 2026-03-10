d756546011c35413030fab692f170eb0cd59972c
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-10 23:14:36 +0200

    error with BASE_URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 const API_KEY = "8f01b3fc069905b3f0215889b15c798d";
-const BASE_URL = "'https://api.themoviedb.org/3";
+const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
