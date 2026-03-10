404d389cdba04df1355aa792c2ec32cf61c64d0e
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-10 22:56:24 +0200

    function to get popular movies from api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,2 +1,9 @@
-const API_KEY = "8f01b3fc069905b3f0215889b15c798d"
-const BASE_URL = "'https://api.themoviedb.org/3"
+const API_KEY = "8f01b3fc069905b3f0215889b15c798d";
+const BASE_URL = "'https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+
+    return data.results;
+};
