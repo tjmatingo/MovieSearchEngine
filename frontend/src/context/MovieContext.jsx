321cd623db7903b542b24b9095e0ff44d07526de
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-03-11 23:50:37 +0200

    context functions for adding removing and presence check creation

--- frontend/src/context/MovieContext.jsx
+++ frontend/src/context/MovieContext.jsx
@@ -8,10 +8,46 @@ export const useMovieContext  = () => useContext(MovieContext)
 // children property allows the function to have inner components 
 export const MovieProvider = ({children}) => {
     const [favourites, setFavourites] = useState([])
-    
+
+    // localstorage allows storage in local browser
+    useEffect (() => {
+        const storedFavs = localStorage.getItem("favourites");
+
+        if (storedFavs) setFavourites(JSON.parse(storedFavs));
+    }, []);
 
 
-    return <MovieContext.Provider>
+    // fetching JSON array of favourites
+    useEffect(() => {
+        localStorage.setItem('favourites', JSON.stringify(favourites))
+    }, [favourites]);
+
+    // function to add from  array 
+    const addToFavourites = (movie) => {
+        // update state to array to add value to array 
+        setFavourites(prev => [...prev, movie])
+    };
+
+    // function to remove from  array 
+    const removeFromFavourites = (movieID) => {
+        setFavourites(prev => prev.filter(movie => movie.ID !== movieID));
+    };
+
+    // function to check existence
+    const isFavourites = (movieID) => {
+        // return true or false to a movie being in favourites
+        return favourites.some(movie => movie.ID === movieID)
+    };
+
+    // to make these values available to children
+    const values = {
+        favourites, 
+        addToFavourites, 
+        removeFromFavourites, 
+        isFavourites
+    }
+
+    return <MovieContext.Provider value={values}>
         {children}
     </MovieContext.Provider>
 }
